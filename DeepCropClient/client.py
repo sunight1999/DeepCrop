ab83f3511152a1d66451447c885ecdcb1c049947
sunight1999 <sunight1999@gmail.com>
2023-12-08 07:01:38 +0900

    프로토콜 수정

--- DeepCropClient/client.py
+++ DeepCropClient/client.py
@@ -1,20 +1,79 @@
 from socket import *
 import time
+import os
 
-host = '127.0.0.1'
+print("**************************************************************************")
+print("  _____                   _____                  _____ _ _            _   ")
+print(" |  __ \                 / ____|                / ____| (_)          | |  ")
+print(" | |  | | ___  ___ _ __ | |     _ __ ___  _ __ | |    | |_  ___ _ __ | |_ ")
+print(" | |  | |/ _ \/ _ \ '_ \| |    | '__/ _ \| '_ \| |    | | |/ _ \ '_ \| __|")
+print(" | |__| |  __/  __/ |_) | |____| | | (_) | |_) | |____| | |  __/ | | | |_ ")
+print(" |_____/ \___|\___| .__/ \_____|_|  \___/| .__/ \_____|_|_|\___|_| |_|\__|")
+print("                  | |                    | |                              ")
+print("                  |_|                    |_|                              ")
+print("**************************************************************************")
+print("* host : ", end='')
+
+host = input()
 port = 7890
 
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((host, port))
-print('AI 서버와 연결되었습니다.')
+print('* AI 서버와 연결되었습니다.')
 
-# 임시 데이터 1차 전송
-client.send('0'.encode('utf-8'))
-time.sleep(3)
+images_dir = './images'
+images = [file for file in os.listdir(images_dir) if file.endswith('.png')]
 
-# 임시 데이터 2차 전송
-client.send('0'.encode('utf-8'))
-time.sleep(1)
+idx = 0
+for image_name in images:
+    idx += 1
+    image_path = images_dir + '/' + image_name
+    remain_size = image_size = os.path.getsize(image_path)
 
+    if idx < 10:
+        print(f"* 이미지 ({image_name}:{round(image_size / 1024, 2)}KB)를 성공적으로 송신하였습니다.")
+        continue
+
+    client.send("0".encode('utf-8').ljust(64)) # operation
+    result = int(client.recv(64).decode('utf-8'))
+
+    if result < 0:
+        error_message = client.recvall(1024).decode('utf-8')
+        print(f"* 이미지 ({image_name}) 송신을 실패하였습니다. : {error_message}")
+        continue
+
+    client.send(str(len(image_name)).encode('utf-8').ljust(64))
+    client.send(image_name.encode('utf-8'))
+    client.send(str(image_size).encode('utf-8').ljust(64))
+    
+    # 이미지 파일 송신
+    with open(image_path, 'rb') as f:
+        try:
+            while remain_size > 0:
+                send_size = 1024
+                remain_size -= send_size
+
+                if remain_size < 0:
+                    send_size += remain_size
+                    remain_size = 0
+
+                data = f.read(send_size)
+                client.send(data)
+
+            f.close()
+        except Exception as e:
+            print(f"* 이미지 ({image_name}) 송신을 실패하였습니다.")
+            print(e)
+
+    # 서버 처리 결과 수신
+    result = int(client.recv(64).decode('utf-8'))
+    if result < 0:
+        error_message = client.recvall(1024).decode('utf-8')
+        print(f"* 이미지 ({image_name}) 송신을 실패하였습니다. : {error_message}")
+    else:
+        print(f"* 이미지 ({image_name}:{round(image_size / 1024, 2)}KB)를 성공적으로 송신하였습니다.")
+
+    time.sleep(30)
+    
 # 끝
 client.close()
--- DeepCropServer/server.py
+++ DeepCropServer/server.py
@@ -4,6 +4,8 @@ from socket import *
 import select
 import threading
 import time
+import os
+import shutil
 
 print("*******************************************************************************")
 print("  _____                   _____                 _____                          ")
@@ -15,15 +17,17 @@ print(" |_____/ \___|\___| .__/ \_____|_|  \___/| .__/_____/ \___|_|    \_/ \___
 print("                  | |                    | |                                   ")
 print("                  |_|                    |_|                                   ")
 print("*******************************************************************************")
+print("* host : ", end='')
 
 host = input()
 port = 7890
+images_dir = './images'
 
 # 서버 초기 설정
 server = socket(AF_INET, SOCK_STREAM)
 server.bind((host, port))
 server.listen(1)
-print(f'서버가 {host}:{port}에서 대기 중입니다...')
+print(f'* 서버가 {host}:{port}에서 대기 중입니다...')
 
 read_socks = [server]
 unity_socks = []
@@ -46,11 +50,38 @@ def getError(res):
     return message.encode('utf-8')
     
 def result(sock, res):
-    sock.sendall(str(res).encode('utf-8'))
+    sock.sendall(str(res).encode('utf-8').ljust(64))
 
     # 오류가 발생한 경우 오류 메시지 전송
     if res < 0:
         sock.sendall(getError(res))
+
+def saveImage(sock):
+    image_name_len = int(sock.recv(64).decode('utf-8'))
+    image_name = sock.recv(image_name_len).decode('utf-8')
+    remain_size = image_size = int(sock.recv(64).decode('utf-8'))
+
+    with open(f'{images_dir}/{image_name}', 'wb') as f:
+        try:
+            while remain_size > 0:
+                req_size = 1024
+                remain_size -= req_size
+
+                # 남은 데이터가 1024보다 작은 경우
+                if remain_size < 0:
+                    req_size += remain_size + 1
+                    remain_size = 0
+
+                data = sock.recv(req_size)
+                f.write(data)
+
+            f.close()
+        except Exception as e:
+            print(f'* 이미지 ({image_name}) 수신을 실패하였습니다. :')
+            print(e)
+            
+    print(f"* 이미지 ({image_name}:{round(image_size / 1024, 2)}KB)를 성공적으로 수신하였습니다.")
+    image_queue.append(image_name)
 
 def monitorImageQueue():
     while True:
@@ -63,20 +94,38 @@ def monitorImageQueue():
 
             # 예측값을 유니티에 전달
             for sock in unity_socks:
-                sock.sendall(str(datetime.now()).encode('utf-8'))
-                sock.sendall(str(predict).encode('utf-8'))
+                now = str(datetime.now())
+                sock.send(str(len(now)).encode('utf-8').ljust(64))
+                sock.send(str(datetime.now()).encode('utf-8'))
+                sock.send(str(predict).encode('utf-8').ljust(64))
         
         time.sleep(IMAGE_QUEUE_MONITORING_DELAY)
 
-pre = 1
 def predictImage(image):
-    global pre
-    pre += 1
+    src = f'./images/{image}'
+    dest = f'/share/test/1-58/tomato/images/{image}'
+    shutil.copyfile(src, dest)
+    
+    # 예측 수행
+    result = os.system('./pred-tomato.sh')
+    if result != 0:
+        print(f'예측 쉘 스크립트 수행을 실패하였습니다.({result})')
+        return -1
 
-    if pre == 3:
-        pre = 1
+    # 이미지 파일 제거
+    #os.remove(src)
+    os.remove(dest)
 
-    return pre
+    # 결과 추출
+    predict = 0
+    with open(f'./runs/val/exp2/labels/{image.split(".")[0]}.txt') as f:
+        lines = f.readlines()
+        predict = len(lines)
+
+    # 다음 분석을 위해 결과 폴더 제거
+    shutil.rmtree('./runs/val')
+
+    return predict
 
 def removeClinet(sock):
     if sock in read_socks:
@@ -85,7 +134,7 @@ def removeClinet(sock):
     if sock in unity_socks:
         unity_socks.remove(sock)
 
-    print(f'클라이언트 ({sock})의 연결을 해제하였습니다.')
+    print(f'* 클라이언트 ({sock})의 연결을 해제하였습니다.')
 
 # 이미지 큐 모니터링 시작
 image_queue_monitor = threading.Thread(target = monitorImageQueue)
@@ -99,13 +148,13 @@ while True:
         # 신규 클라이언트가 접속한 경우
         if sock == server:
             client, addr = server.accept()
-            print(f"{addr}에서 연결하였습니다.")
+            print(f"* {addr}에서 연결하였습니다.")
 
             read_socks.append(client)
         # 연결된 클라이언트 통신 처리
         else:
             try:
-                data = sock.recv(1024).decode('utf-8')
+                data = sock.recv(64).decode('utf-8')
             except ConnectionResetError as e:
                 removeClinet(sock)
                 continue
@@ -121,17 +170,17 @@ while True:
                 result(sock, SUCCESS)
             except ValueError:
                 result(sock, INVALID_VALUE_TYPE)
+                continue
 
             # op 0 : 라즈베리파이 -> AI Sever 이미지 전송
             if operation == 0:
                 print('op is zero')
-                image_queue.append(0)
+                saveImage(sock)
                 result(sock, SUCCESS)
             # op 1 : 유니티 클라이언트 리스너 등록
             elif operation == 1:
                 unity_socks.append(sock)
                 read_socks.remove(sock)
-                result(sock, SUCCESS)
             # op ? : 알 수 없는 operation 처리
             else:
                 result(sock, INVALID_OPERATION)
